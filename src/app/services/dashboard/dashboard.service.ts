7825c8fe0d89ed92057b218722a2eb2e8fe0263c
ahmed <sam@google.com>
2023-04-20 02:12:45 +0100

    init commit

---
+++ src/app/services/dashboard/dashboard.service.ts
@@ -0,0 +1,33 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { SystemCpu } from "src/app/models/system-cpu";
+import { SystemHealth } from "src/app/models/system-health";
+import { environment } from "src/environments/environment";
+
+@Injectable({
+  providedIn: "root",
+})
+export class DashboardService {
+  private SERVER_URL = environment.apiBaseUrl + "/actuator";
+
+  constructor(private http: HttpClient) {}
+
+  public getHttpTraces(): Observable<any> {
+    return this.http.get<any>(`${this.SERVER_URL}/httptrace`);
+  }
+
+  public getSystemHealth(): Observable<SystemHealth> {
+    return this.http.get<SystemHealth>(`${this.SERVER_URL}/health`);
+  }
+
+  public getSystemCpu(): Observable<SystemCpu> {
+    return this.http.get<SystemCpu>(
+      `${this.SERVER_URL}/metrics/system.cpu.count`
+    );
+  }
+
+  public getProcessUptime(): Observable<any> {
+    return this.http.get<any>(`${this.SERVER_URL}/metrics/process.uptime`);
+  }
+}
